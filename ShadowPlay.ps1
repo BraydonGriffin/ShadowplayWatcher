dba9cb8001b6b5ff0a7e89ab5798e832dc50cb05
0xInSomnia <90873368+0xInSomnia@users.noreply.github.com>
2021-09-28 13:26:52 +1000

    Initial script upload

---
+++ ShadowPlay.ps1
@@ -0,0 +1,22 @@
+﻿$watcher = New-Object System.IO.FileSystemWatcher;
+$watcher.Path = "C:\Shadowplay";
+$watcher.Filter = "*.mp4";
+$watcher.EnableRaisingEvents = $true;
+$watcher.IncludeSubdirectories = $true;
+$destination = "Z:\Shadowplay";
+
+$action = {
+    $path = $Event.SourceEventArgs.Name;
+    Start-Sleep -Seconds 5
+    $pathArray = $path.Split("\")
+    $folder = $pathArray[0]
+    $dst = "$($destination)\$($folder)"
+    New-Item -ItemType Directory -Force -Path "$($dst)"
+    Copy-Item -Path $Event.SourceEventArgs.FullPath -Destination "$($dst)\$($folder)_$(get-date -f yyyy-MM-dd-HHmmss).mp4";
+}
+
+Unregister-Event -SourceIdentifier "Shadowplay"
+Register-ObjectEvent -InputObject $watcher -EventName "Created" -SourceIdentifier "Shadowplay" -Action $action | Out-Null;
+Add-Type -AssemblyName System.Drawing;
+Remove-Item -Path (Join-Path $watcher.Path '\*') -Recurse
+while ($true) { Wait-Event -SourceIdentifier "Shadowplay"; };
